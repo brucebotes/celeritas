c6bbf395aa663d57eea3be399a2c81e83cc8bc29
Bruce Botes <botes.bruce@gmail.com>
2024-04-20 02:26:23 +0200

    switch id to UUID for sqlite (templates)

--- cmd/cli/templates/migrations/auth_tables.sqlite3.sql
+++ cmd/cli/templates/migrations/auth_tables.sqlite3.sql
@@ -1,41 +1,40 @@
--- THE ID's FIELDS MUST BE CONVERTED TO UUID TO BE COMPATIBLE WITH CELERITAS - SEE POSTGRES
 DROP TABLE IF EXISTS "users";
 
 CREATE TABLE "users" (
-	"id"	INTEGER,
+	"id"	TEXT NOT NULL,
 	"first_name"	TEXT NOT NULL,
 	"last_name"	TEXT NOT NULL,
 	"user_active"	INTEGER NOT NULL DEFAULT '0',
 	"access_level"	INTEGER NOT NULL DEFAULT '3',
-	"email"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
 	"password"	TEXT NOT NULL,
 	"deleted_at"	DATETIME,
 	"created_at"	DATETIME DEFAULT current_timestamp,
 	"updated_at"	DATETIME DEFAULT current_timestamp,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 DROP TABLE IF EXISTS "remember_tokens";
 
 CREATE TABLE "remember_tokens" (
-	"id"	INTEGER,
-	"user_id"	INTEGER NOT NULL,
+	"id"	TEXT NOT NULL,
+	"user_id"	TEXT NOT NULL,
 	"remember_token"	TEXT NOT NULL,
 	"created_at"	DATETIME DEFAULT current_timestamp,
 	"updated_at"	DATETIME DEFAULT current_timestamp,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 DROP TABLE IF EXISTS "tokens";
 
 CREATE TABLE "tokens" (
-	"id"	INTEGER,
-	"user_id"	INTEGER NOT NULL,
+	"id"	TEXT NOT NULL,
+	"user_id"	TEXT NOT NULL,
 	"email"	TEXT NOT NULL,
 	"token"	TEXT NOT NULL,
 	"token_hash"	BLOB,
 	"created_at"	DATETIME DEFAULT current_timestamp,
 	"updated_at"	DATETIME DEFAULT current_timestamp,
 	"expiry"	DATETIME,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
